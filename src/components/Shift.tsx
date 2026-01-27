0ad565940cd0c778411e16703e2fedd9c1151666
Ve4er <terew411@gmail.com>
2026-01-27 22:40:44 +0300

    новое отображение смены

--- src/components/Shift.tsx
+++ src/components/Shift.tsx
@@ -18,10 +18,23 @@ interface ShiftProps {
 export default function Shift({ data }: ShiftProps) {
     const [isVisible, setisVisible] = useState(false);
     const { timeStart, timeEnd, shopName, revenue, cheks, id } = data;
-    const styleWorkingDayInput = ' bg-green-50  rounded text-center hover:bg-green-200'
+    const styleWorkingDayInput = ' text-center '
     const [deleteShift] = useDeleteShiftMutation();
 
     const handlerDelete = async () => { await deleteShift({ shiftId: id }) }
+
+    function getHoursDiff() {
+        const setFormat = (num: number) => num.toString().padStart(2, '0')
+        const [a, b] = [timeEnd, timeStart] as unknown as number[]
+        const diffMs = Math.abs(a - b);
+        const diffHours = diffMs / (1000 * 60 * 60);
+        const rawHH = Number(diffHours);
+        const hh = Math.floor(rawHH);
+        const mm = Math.ceil((rawHH - hh) * 60);
+        if (mm == 60) return `${setFormat(hh + 1)}ч 00м`
+        return `${setFormat(hh)}ч ${setFormat(mm)}м`
+    }
+
 
     return (
         // grid grid-cols-[0fr_1fr_1fr_1fr_1fr]
@@ -31,12 +44,12 @@ export default function Shift({ data }: ShiftProps) {
                 <BiTrash size={18} onClick={handlerDelete} className="ButtonIcon BI2 mr-1 " />
                 <BiPen size={18} onClick={() => { setisVisible(!isVisible) }} className="ButtonIcon BI2 mr-1 " />
                 <DateInput onInput={() => { }} className={styleWorkingDayInput} dateDefault={timeStart} />
-                —
-                <DateInput onInput={() => { }} className={styleWorkingDayInput} dateDefault={timeEnd} />
+                {getHoursDiff()}
+                {/* <DateInput onInput={() => { }} className={styleWorkingDayInput} dateDefault={timeEnd} /> */}
             </div>
-            <input className={'w-24' + styleWorkingDayInput} type="text" disabled value={shopName} />
-            <input className={'w-12' + styleWorkingDayInput} type="value" disabled value={revenue} />
-            <input className={'w-12' + styleWorkingDayInput} type="value" disabled value={cheks} />
+            <span><span className={'w-24' + styleWorkingDayInput} >{shopName}</span>🏪</span>
+            <span><span className={'w-12' + styleWorkingDayInput} >{revenue}</span>💲</span>
+            <span><span className={'min-w-12' + styleWorkingDayInput} >{cheks}</span>📄</span>
         </div>
     )
 }

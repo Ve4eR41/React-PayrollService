f36801aa8d2d4e17e381cc98aeac3f64044d77c6
Ve4er <terew411@gmail.com>
2026-04-07 22:19:30 +0300

    SummaryShiftPanel style fix

--- src/components/SummaryShiftPanel.tsx
+++ src/components/SummaryShiftPanel.tsx
@@ -13,10 +13,26 @@ interface SummaryShiftPanelProps {
 function SummaryShiftPanel({ className, summary }: SummaryShiftPanelProps) {
     return (
         <Panel className={`flex justify-around gap-4 my-4 text-xs sm:text-[16px] ${className || ''}`}>
-            <span className="bg-green rounded-full py-1 px-3">📆Смен: {summary.q}</span>
-            <span className="bg-green rounded-full py-1 px-3">⌛Отработанно: {Math.round(summary.workTime / (1000 * 60 * 60))}ч.</span>
-            <span className="bg-green rounded-full py-1 px-3">💲Выркучка: {summary.revenue}</span>
-            <span className="bg-green rounded-full py-1 px-3">📄Чеков: {summary.cheks}</span>
+            <span className="bg-green rounded-full py-1 px-3">
+                <div className="text-center">📆</div>
+                Смен:
+                <div className="text-center">{summary.q}</div>
+            </span>
+            <span className="bg-green rounded-full py-1 px-3">
+                <div className="text-center">⌛</div>
+                Время:
+                <div className="text-center"> {Math.round(summary.workTime / (1000 * 60 * 60))}ч.</div>
+            </span>
+            <span className="bg-green rounded-full py-1 px-3">
+                <div className="text-center"> 💲</div>
+                Выркучка:
+                <div className="text-center">{summary.revenue}</div>
+            </span>
+            <span className="bg-green rounded-full py-1 px-3">
+                <div className="text-center">📄</div>
+                Чеков:
+                <div className="text-center">{summary.cheks}</div>
+            </span>
         </Panel>
     );
 }

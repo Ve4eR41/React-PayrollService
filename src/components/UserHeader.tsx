32617e3f5f7db10faf0d6bb630be8ae2797114f6
Ve4er <terew411@gmail.com>
2026-04-07 22:09:43 +0300

    UserHeader minor fix

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -17,7 +17,7 @@ function UserHeader() {
 
     return (
         <>
-            <div className=" text-white bg-green-600 h-[7vh] w-full rounded-b-4xl rounded-l-4xl pr-6  pl-2 flex items-center text-sm">
+            <div className=" text-white bg-green-600 md:h-[7vh] h-[10vh] w-full rounded-b-4xl rounded-l-4xl pr-6 overflow-hidden pl-2 flex items-center text-sm">
 
                 <div className=" rounded-full h-[80%] aspect-square flex items-center justify-center overflow-hidden">
                     <img className="w-full " src={defaultAva} alt="" />

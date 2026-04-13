eb96a23fcc9999f009a33e0d93235f9d1a565cd3
Ve4er <terew411@gmail.com>
2026-04-13 21:38:26 +0300

    refactor(SummaryShiftPanel): отображение значении

--- src/components/SummaryShiftPanel.tsx
+++ src/components/SummaryShiftPanel.tsx
@@ -13,20 +13,20 @@ interface SummaryShiftPanelProps {
 function SummaryShiftPanel({ className, summary }: SummaryShiftPanelProps) {
     const { cheks, q, revenue, workTime } = summary
 
-    const el = (icon: string, label: string, value: number) => {
+    const el = (icon: string, label: string, value: number, type = '') => {
         return <span className="bg-green rounded-full py-1 px-3">
             <div className="text-center">{icon}</div>
             {label}:
-            <div className="text-center">{value.toLocaleString('ru-RU')}</div>
+            <div className="text-center">{value.toLocaleString('ru-RU')}{type}</div>
         </span>
     }
     return (
         <Panel className={`flex justify-around gap-4 my-4 text-xs sm:text-[16px] ${className || ''}`}>
             {el('📆', 'Смен', q)}
-            {el('⌛', 'Время', workTime)}
-            {el('💲', 'Выркучка', revenue)}
+            {el('⌛', 'Время', Math.round(workTime / (1000 * 60 * 60)),'ч.')}
+            {el('💲', 'Выркучка', revenue,'р.')}
             {el('📚', 'Чеков', cheks)}
-            {el('📄', 'Ср.Чек', Math.round(revenue / cheks))}
+            {el('📄', 'Ср.Чек', Math.round(revenue / cheks) || 0)}
         </Panel>
     );
 }

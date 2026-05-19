46403085be36598a396f3f80f5d8321ada3d6af6
Ve4er <terew411@gmail.com>
2026-05-19 23:10:50 +0300

    fix: CheckPayroll

--- src/components/Check/CheckPayroll.tsx
+++ src/components/Check/CheckPayroll.tsx
@@ -1,18 +1,18 @@
 import { useGetPayrollQuery } from "../../store/apis/payroll";
-import { getStatusFetch } from "../../utils/utils";
+import { getStartMouth, getStatusFetch } from "../../utils/utils";
 import SkeletPanel from "../Loader/SkeletPanel";
 import CheckBase from "./CheckBase";
 
 export default function CheckPayroll() {
     const { data, isLoading, error } = useGetPayrollQuery(
         {
-            "timeStart": new Date("2026-05-01T07:44:35.677Z"),
+            "timeStart": getStartMouth(new Date()),
             "timeEnd": new Date("2099-09-30T07:44:35.677Z")
         }
     )
 
 
-    
+
     if (!data || isLoading) return <SkeletPanel />
     if ([403, 401].includes(getStatusFetch(error))) return <></>
     return <>
